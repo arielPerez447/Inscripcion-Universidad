9d2fc25e776cbd4249639d5de71c37172f8dfc62
ArielEzequielPerez <ariperez@frba.utn.edu.ar>
2022-07-04 22:43:44 -0300

    Falta hacer la migración

---
+++ Models/Dominio/HistorialAcademico.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace SistemaDeInscripcionUniversitario.Models.Dominio
+{
+    public class HistorialAcademico : EntityBase
+    {
+        public int IdMateria { get; set; }
+        public virtual Materia Materias {get; set;}
+        public DateTime Fecha_Examen { get; set; }
+        public Decimal Nota { get; set; }
+    }
+}
